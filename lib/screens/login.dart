bf53722684bd391e9afbe680fa74e38a4147bde8
Gabriel Chioquetti <gabrielchioquetti@gmail.com>
2026-05-24 18:49:27 -0300

    feat: criar estrutura inicial da tela de login

---
+++ lib/screens/login.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class TelaLogin extends StatefulWidget {
+  @override
+  State<TelaLogin> createState() => _TelaLogin();
+}
+
+class _TelaLogin extends State<TelaLogin> {
+  
+  // Controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Bem Vindo!",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Faça login para continuar",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Digite seu email",
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  filled: true,
+                ),
+              ),
+              TextField(
+                controller: senhaController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Digite sua senha",
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  filled: true,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child:  ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12), // Alinhe o raio com o do seu TextField
+                      ),
+                      backgroundColor: Colors.green, 
+                    ),
+                  onPressed: () {
+                    String email = emailController.text;
+                    String senha = senhaController.text;
+                    print(email);
+                    print(senha);
+                  }, 
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ),
+              TextButton(onPressed: () {}, child: Text("Esqueci minha senha")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
