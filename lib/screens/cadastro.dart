22ba39f279db53713e4c616f6896fe8ec2177f37
Gabriel Chioquetti <gabrielchioquetti@gmail.com>
2026-05-25 11:21:42 -0300

    feat: criar estrutura inicial da tela de cadastro

--- lib/screens/cadastro.dart
+++ lib/screens/cadastro.dart
@@ -1,1 +1,132 @@
 import 'package:flutter/material.dart';
+
+class TelaCadastro extends StatefulWidget {
+  @override
+  State<TelaCadastro> createState() => _TelaCadastro();
+}
+
+class _TelaCadastro extends State<TelaCadastro> {
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+  final TextEditingController senhaIgualController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Criar Conta",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Preencha os dados para se cadastrar",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: nomeController,
+                decoration: InputDecoration(
+                  labelText: "Digite seu nome completo",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                ),
+              ),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Digite seu email",
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                ),
+              ),
+              TextField(
+                controller: senhaController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Digite sua senha",
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                ),
+              ),
+              TextField(
+                controller: senhaIgualController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Confirme sua senha",
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        12,
+                      ), // Alinhe o raio com o do seu TextField
+                    ),
+                    backgroundColor: Colors.green,
+                  ),
+                  onPressed: () {
+                    String nome = nomeController.text;
+                    String email = emailController.text;
+                    String senha = senhaController.text;
+                    String senhaIgual = senhaIgualController.text;
+                    print(nome);
+                    print(email);
+                    print(senha);
+                    print(senhaIgual);
+                  },
+                  child: Text(
+                    "Cadastrar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.spaceBetween, // Alinha um em cada ponta
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // Alinha verticalmente ao centro
+                children: [
+                  Text(
+                    "Criar Conta",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  TextButton(onPressed: () {}, child: Text("Entrar")),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
